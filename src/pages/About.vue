b6c78024abcd49e5dca4e0647042ef877058ed44
Vladimir Tolmachev <morettibrian01@gmail.com>
2022-05-27 12:02:04 +0300

    VueCourse-Update

---
+++ src/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Я бомж Василий</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Добро пожаловать в BomgGang</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
